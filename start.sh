74c4a9170a4a81764abe281907303396405645bd
TebaHuido <baase.hui@gmail.com>
2025-01-07 14:17:46 -0300

    testing4

--- start.sh
+++ start.sh
@@ -10,15 +10,31 @@ cleanup() {
 
 trap cleanup SIGINT SIGTERM
 
+# Configurar autocompletado de Angular de manera silenciosa antes de iniciar servicios
+if ! grep -q "ng completion" ~/.bashrc; then
+    echo "Configurando autocompletado de Angular..."
+    ng completion > /tmp/ng_completion
+    cat /tmp/ng_completion >> ~/.bashrc
+    source ~/.bashrc
+    rm /tmp/ng_completion
+fi
+
 # Activar entorno virtual e iniciar Django
 cd "$HOME/histologia/django"
 source "$HOME/histologia/django/venv/bin/activate"
 python manage.py runserver 0.0.0.0:8000 &
 DJANGO_PID=$!
 
-# Iniciar Angular
+# Iniciar Angular verificando la instalación
 cd "$HOME/histologia/angular"
-ng serve --host 0.0.0.0 --port 4200 &
+if [ ! -f "package.json" ]; then
+    echo "Error: No se encontró package.json en el directorio de Angular"
+    exit 1
+fi
+
+echo "Iniciando servidor Angular..."
+export NODE_OPTIONS=--max_old_space_size=4096
+ng serve --host 0.0.0.0 --port 4200 --disable-host-check &
 ANGULAR_PID=$!
 
 # Esperar por los procesos
