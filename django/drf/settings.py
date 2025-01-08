d52cde4027f361d884a3c8cd069cf39e9530536f
TebaHuido <baase.hui@gmail.com>
2025-01-08 11:27:04 -0300

    test

--- django/drf/settings.py
+++ django/drf/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
+    'api',  # Asegúrate de que la app 'api' esté incluida
     'rest_framework',
     'corsheaders'
 ]
